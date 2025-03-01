6d042522ef19c2211b04ab0542a11b779e1ca5e9
Alexia Cardoso <alexiacardoso@suasfacil.com>
2025-03-01 10:52:01 -0300

    first commit

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TarefaController extends Controller
+{
+
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        if (Auth::check()) {
+            $id = Auth::user()->id;
+            $name = Auth::user()->name;
+            $email = Auth::user()->email;
+
+            return "usuario $id com nome $name e email $email";
+        }
+        else echo 'não logado';
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}

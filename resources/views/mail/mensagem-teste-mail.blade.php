4e9a0cd39f90fa8314487f36338b50848236b105
Alexia Cardoso <alexiacardoso@suasfacil.com>
2025-03-03 17:22:43 -0300

    inicia email

---
+++ resources/views/mail/mensagem-teste-mail.blade.php
@@ -0,0 +1,12 @@
+<x-mail::message>
+# Introdução
+
+Corpo da mensagem.
+
+<x-mail::button :url="''">
+Clique aqui
+</x-mail::button>
+
+Atenciosamente,<br>
+{{ config('app.name') }}
+</x-mail::message>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTesteMail;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,3 +11,12 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('tarefa',App\Http\Controllers\TarefaController::class); //->middleware('auth');
+
+Route::get('mensagem-teste', function() {
+    Mail::to('kpopermoomoo@gmail.com')->send(new MensagemTesteMail());
+    return 'email enviado com sucesso';
+});
+
+Route::get('mensagem-view-teste', function() {
+    return new MensagemTesteMail();
+});
